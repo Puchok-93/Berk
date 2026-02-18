25107e7db7ec93d0b7ba3c33421684895338b932
Павел <puchok@Mac-mini-Pavel.local>
2026-02-19 02:17:32 +0300

    create main

---
+++ js/script.js
@@ -0,0 +1,43 @@
+function initReviewSlider() {
+    const reviewSlider = document.getElementById('reviews-slider');
+    if(!reviewSlider) return;
+
+    const slides = reviewSlider.querySelectorAll('.swiper-slide');
+    
+    const swiper = new Swiper(reviewSlider, {
+        loop: slides.length > 3 ? true : false,
+        slidesPerView: 3,
+        observer: true,
+        observeParents: true,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next', 
+            prevEl: '.swiper-button-prev', 
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: false,
+        }
+    });
+}
+
+function initNewsSlider() {
+    const newsSlider = document.getElementById('news-slider');
+
+    if(!newsSlider) return;
+
+    const swiper = new Swiper(newsSlider, {
+        loop: true,
+        slidesPerView: 4,
+        observer: true,
+        observeParents: true,
+        spaceBetween: 20,
+    })
+}
+
+function init() {
+    initReviewSlider();
+    initNewsSlider();
+}
+
+document.addEventListener('DOMContentLoaded', init);
